ca31a9065d7342e1bee520118457d1f45e4621d5
SHOBHITSAGAR <67162728+shobhitsagar74@users.noreply.github.com>
2022-01-03 15:59:04 +0530

    Add files via upload

---
+++ js/mein.js
@@ -0,0 +1,17 @@
+
+//for typing text automatic runngr
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.9/typed.min.js" async defer></script>')
+window.onload = function () {
+    console.log("loaded")
+    var typed = new Typed('#typed', {
+        strings: ["Welcome to Shobhit Sagar", "Learn Python", "Web development", "hacking", "Machine Learning", "And tips and tricks :-"],
+        backSpeed: 15,
+        smartBackspace: true,
+        backDelay: 1200,
+        startDelay: 1000,
+        typeSpeed: 25,
+        loop: true,
+    });
+};
+//closs java for typing text automatic runngr
+
